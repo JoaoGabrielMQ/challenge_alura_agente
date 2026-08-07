888fbde7c050884d27ce5ddb47bcf861f577a111
João Gabriel <joaog_queiroz@hotmail.com>
2026-08-07 17:53:08 -0300

    refatorando o agente para persistir memoria

--- agente.py
+++ agente.py
@@ -1,31 +1,31 @@
 from ferramentas import fazer_buscar
 from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
-from typing import TypedDict
+from langgraph.checkpoint.memory import InMemorySaver
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-class AgentState(TypedDict):
-    user_query: str
-    web_answer: str
-    scientific_answer: str
-    final_answer: str
-
-
-def funcao_agente_web(state: AgentState) -> dict:
-    tools = [fazer_buscar]
-    llm = ChatGoogleGenerativeAI(api_key=os.getenv('GEMINI_API_KEY'), model="gemini-3.1-flash-lite")
-    system_prompt = """Você é um assistente técnico prestativo. Você tem acesso a ferramentas para consultar documentos locais.
+class AgentePesquisa:
+    def __init__(self):
+        self.tools = [fazer_buscar]
+        self.llm = ChatGoogleGenerativeAI(api_key=os.getenv('GEMINI_API_KEY'), model="gemini-3.1-flash-lite")
+        self.system_prompt = """Você é um assistente técnico prestativo. Você tem acesso a ferramentas para consultar documentos locais.
                     fazer_buscar: Use essa ferramente para fazer as buscas.
                     Se não conseguir uma resposta com fazer_buscar retorne 'Não encontrei a resposta.'
                     Responda SEMPRE em português brasileiro com base no que a ferramenta retornar."""
+        self.agente = create_agent(model=self.llm, tools=self.tools, system_prompt=self.system_prompt, checkpointer=InMemorySaver())
 
-    agente = create_agent(model=llm, tools=tools, system_prompt=system_prompt)
+    def funcao_agente(self, pergunta) -> dict:
+        configuracao = {"configurable": {"thread_id": "conversa_aprendizado_01"}}
 
-    resposta = agente.invoke({"messages": [("user", state["Qual o checklist de WCAG 2.1 AA?"])]})
-    return resposta['messages'][-1].content
+        resposta = self.agente.invoke({"messages": [("user", pergunta)]}, config=configuracao)
+        return resposta['messages'][-1].content
 
-print(funcao_agente_web())
+
+agente = AgentePesquisa()
+
+print(agente.funcao_agente(pergunta="Qual o checklist de WCAG 2.1 AA?"))
+print(agente.funcao_agente(pergunta="Quantos itens são na pergunta anterior?"))
